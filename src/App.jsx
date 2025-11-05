e95ea02819bc335c48a18c0ae325db190b44ded0
Viktoriya <130046286+Kuron13@users.noreply.github.com>
2025-11-05 18:27:46 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import './App.css';
+import { Stars } from './components/Stars.jsx';
+
+function App() {
+  
+  return (
+    <Stars count={1}/>
+  )
+}
+
+export default App
