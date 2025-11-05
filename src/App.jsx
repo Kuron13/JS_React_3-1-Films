343a989c4697720157a8e2e7d5fda04c360732c8
Viktoriya <130046286+Kuron13@users.noreply.github.com>
2025-11-05 19:34:27 +0500

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,30 @@ import { Stars } from './components/Stars.jsx';
 function App() {
   
   return (
-    <Stars count={1}/>
+    <div>
+      <div> Рейтинг: 0
+        < Stars count={0} />
+      </div>
+      <div> Рейтинг: 1
+        < Stars count={1} />
+      </div>
+      <div> Рейтинг: 3
+        < Stars count={3} />
+      </div>
+      <div> Рейтинг: 3.7
+        < Stars count={3.7} />
+      </div>
+      <div> Рейтинг: 5
+        < Stars count={5} />
+      </div>
+      <div> Рейтинг: 8
+        < Stars count={8} />
+      </div>
+      <div> Рейтинг: Текст
+        < Stars count={'Текст'} />
+      </div>
+    </div>
   )
 }
 
-export default App
+export default App;
