ea501bb5c2768b2d5c747e83f68e9ff3aef9f908
Viktoriya <130046286+Kuron13@users.noreply.github.com>
2025-11-06 16:29:05 +0500

    Update and rename Stars.jsx to Stars.tsx

--- src/components/Stars.tsx
+++ src/components/Stars.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
 
-export function Stars({ count = 0 }) {
+interface StarsProps {
+  count?: number;
+}
+
+export function Stars({ count = 0 }: StarsProps) {
 
   return (
     <ul className="card-body-stars u-clearfix">
-      {typeof count === 'number' && count >= 1 && count <= 5 && [...Array(Math.round(count))].map((_, i) => <Star i={i} />)}
+      {typeof count === 'number' && count >= 1 && count <= 5 && [...Array(Math.round(count))].map((_, i) => <Star key={i} />)}
     </ul>
   );
 
 }
 
-export function Star({ i = 0 }) {
+export function Star() {
 
   return (
-    <li className="star-li" key={i}>
+    <li className="star-li">
       <svg fill="#D3BCA2" height="28" viewBox="0 0 18 18" width="28" xmlns="http://www.w3.org/2000/svg">
         <path d="M9 11.3l3.71 2.7-1.42-4.36L15 7h-4.55L9 2.5 7.55 7H3l3.71 2.64L5.29 14z"/>
         <path d="M0 0h18v18H0z" fill="none"/>
